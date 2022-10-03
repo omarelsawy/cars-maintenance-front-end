ebf99a04c57f84fce3af8f7e5537a2ce05312036
omar elsawy <omar.elsawy012@gmail.com>
2022-10-03 16:05:03 +0200

    single maintenance

--- src/pages/SingleMaintenance.js
+++ src/pages/SingleMaintenance.js
@@ -1,12 +1,43 @@
-import { Row } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Card, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
+import {API_URL} from '../utils/Constant';
 
-const SingleMaintenance = ({ maintenance }) => {
+const SingleMaintenance = () => {
 
     const params = useParams();
+    const navigate = useNavigate();
+
+    const [ maintenance, setMaintenance ] = useState({});
+
+    async function fetchMaintenance() {
+        let response = await fetch(`${API_URL}/maintenance/${params.id}`,{
+            headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
+        });
+
+        let responseJson = await response.json();
+        let status = response.status;
+
+        if(status === 200){
+            const maintenanceRes = responseJson?.data?.singleMaintenance;
+            setMaintenance(maintenanceRes);
+        }
+        else if(status === 401){
+            localStorage.removeItem("token");
+            navigate('/login');
+        }
+        else{
+            let error = responseJson?.data?.error
+            error && alert(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchMaintenance();
+    }, []);
 
     return (
         <>
@@ -14,7 +45,20 @@ const SingleMaintenance = ({ maintenance }) => {
             <SideBar />
             <Row className='m-3 float-end' style={{ width: '80%' }}>
 
-                single
+            <Card>
+            <Card.Body>
+                <Card.Title>{ maintenance.car?.name }</Card.Title>
+                <Card.Text>
+                    { new Date(maintenance.createdAt).toLocaleDateString() }
+                </Card.Text>
+                <Card.Text>
+                    { maintenance.description }
+                </Card.Text>
+                <Card.Text>
+                    { maintenance.price } $
+                </Card.Text>
+            </Card.Body>
+            </Card>
                 
             </Row>
 
