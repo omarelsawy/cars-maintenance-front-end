5497d875006901525982b616db7d597b42c45b93
omar elsawy <omar.elsawy012@gmail.com>
2022-10-12 15:00:04 +0200

    live back end

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,3 +1,5 @@
 
-export const API_URL = 'http://localhost:3001';
-export const API_URL_COMPANY = `http://localhost:3001/company/${localStorage.getItem('slug')}`;
+//export const API_URL = 'http://localhost:3001';
+export const API_URL = 'https://maintenance-app-node.herokuapp.com';
+
+export const API_URL_COMPANY = `https://maintenance-app-node.herokuapp.com/company/${localStorage.getItem('slug')}`;
